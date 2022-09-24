fce5c5799be63d2bc71e454b010723ff1bb03d57
Fiodar Yuzhyk <fiodaryuzhyk@Fiodars-MacBook-Air.local>
2022-09-24 22:28:09 +0200

    homeScreen

--- slash_2022_2/lib/main.dart
+++ slash_2022_2/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 import 'dart:io';
+
+import 'screens/home.dart';
+import 'utils/colors.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -14,68 +19,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-            '#ff6666',
-            'cancel',
-            true,
-            ScanMode.BARCODE,
-          );
-          debugPrint(barcodeScanRes);
-          postRequest();
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        transitionDuration: const Duration(milliseconds: 500),
+        defaultTransition: Transition.fadeIn,
+        getPages: [
+          GetPage(
+            name: '/',
+            page: () => const HomeScreen(),
+            transition: Transition.fadeIn,
+          ),
+        ],
+        theme: ThemeData(
+          primaryColor: green,
+          backgroundColor: vanilla,
+          scaffoldBackgroundColor: vanilla,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: vanilla,
+            surfaceTintColor: vanilla,
+            shadowColor: shadow
+          ),
+          textTheme: GoogleFonts.soraTextTheme(Theme.of(context).textTheme),
+          useMaterial3: true
+        ));
   }
 }
 
